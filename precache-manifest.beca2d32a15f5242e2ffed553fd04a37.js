e4e25b769f0ad0e0866b586ebfb7f0ef7156a908
Kanstantsin Shchuchkin <kanstantsin.shchuchkin@pricehubble.com>
2022-03-20 19:13:13 +0500

    Updates

---
+++ precache-manifest.beca2d32a15f5242e2ffed553fd04a37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8874c0a83d005f30f928cf8beb814ca3",
+    "url": "/react-monster-exer/index.html"
+  },
+  {
+    "revision": "be7cca496d1887a778df",
+    "url": "/react-monster-exer/static/css/main.7bc0d738.chunk.css"
+  },
+  {
+    "revision": "6f3db001adba5c2bf77c",
+    "url": "/react-monster-exer/static/js/2.b991c77e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-monster-exer/static/js/2.b991c77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c294149f9c13ae4a0cc",
+    "url": "/react-monster-exer/static/js/3.c5e2c00e.chunk.js"
+  },
+  {
+    "revision": "be7cca496d1887a778df",
+    "url": "/react-monster-exer/static/js/main.aef98c83.chunk.js"
+  },
+  {
+    "revision": "56c0d477d4b2420ccae6",
+    "url": "/react-monster-exer/static/js/runtime-main.74eb2bf1.js"
+  }
+]);
